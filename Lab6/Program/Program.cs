60e139a2a735250924ab29b91610f708e2242a37
Dima Shmundiak <dimashmund@gmail.com>
2017-04-20 22:51:40 +0300

    Lab6

---
+++ Lab6/Program/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("\nВведiть Exit, щоб вийти або введiть рядок для перевiрки");
+                Console.Write("Введiть рядок: ");
+                string str = Console.ReadLine();
+                if (str == "Exit") break;
+                if (isLettersOnBeginning(str) || isNumber(str)) Console.WriteLine("Вiдповiдає шаблону");
+                else Console.WriteLine("Не вiдповiдає шаблону");
+            }
+        }
+
+        static bool isNumber(string input)
+        {
+            var regex = new Regex("^[0-9]+$");
+            return regex.IsMatch(input);
+        }
+
+        static bool isLettersOnBeginning(string input)
+        {
+            var regex = new Regex(@"^([a-z])([0-9a-z]+$)");
+            return regex.IsMatch(input);
+        }
+    }
+}
